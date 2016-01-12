51b9e9d817e94aa1b564f4f4931e2236e834d19c
Tomokazu Imai <bobbyjam99@gmail.com>
2016-01-12 17:58:19 +0900

    RestControllerの追加

--- spring-boot-doma/src/main/java/com/example/DemoApplication.java
+++ spring-boot-doma/src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,39 @@
 package com.example;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Autowired
+	ReservationDao reservationDao;
+
+	// 起動時にReservationDao#insertで初期データを投入する
+	@Bean
+	CommandLineRunner runner() {
+		return args -> Arrays.asList("spring", "spring boot", "spring cloud", "doma").forEach(s -> {
+			Reservation r = new Reservation();
+			r.name = s;
+			reservationDao.insert(r);
+		});
+	}
+
+	@RequestMapping(path = "/")
+	List<Reservation> all() {
+		return reservationDao.selectAll();
 	}
 }
