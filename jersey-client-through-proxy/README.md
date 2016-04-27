f7c87e5372d046f43a9ee6ecc544bc9e20f0bc24
Tomokazu IMAI <bobbyjam99@gmail.com>
2016-04-28 01:19:24 +0900

    サーバ実装追加

---
+++ jersey-client-through-proxy/README.md
@@ -0,0 +1,3 @@
+Jersey Client 素振り
+
+サーバ側をSpring Bootで実装。
---
+++ jersey-client-through-proxy/src/main/java/app/controller/HogeController.java
@@ -0,0 +1,14 @@
+package app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HogeController {
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hello" + name;
+    }
+}
