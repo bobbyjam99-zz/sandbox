d663fcffefb9430cef21d6fcc9f7a66097e5a26a
Tomokazu Imai <bobbyjam99@gmail.com>
2017-04-12 21:20:13 +0900

    約分する

--- kotlin-startbook/src/Rational.kt
+++ kotlin-startbook/src/Rational.kt
@@ -1,9 +1,17 @@
 // クラスRationalの定義
-class Rational(val numerator: Int, val denominator: Int) {
+// 約分する
+class Rational(n: Int, d: Int) {
     // イニシャライザで分母0を禁止する
     init {
-        require(denominator != 0, {"denominator must not be null"})
+        require(d != 0, {"denominator must not be null"})
     }
+    private val g = gcd(Math.abs(n), Math.abs(d))
+    val numerator: Int = n / g
+    val denominator: Int = d / g
     // toStringをオーバーライド
     override fun toString(): String = "${numerator}/${denominator}"
+    // 再帰関数
+    tailrec private fun gcd(a: Int, b: Int): Int =
+            if (b == 0) a
+            else gcd(b, a % b)
 }
