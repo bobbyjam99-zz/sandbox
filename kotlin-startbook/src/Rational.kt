ecc53f61d1c4d1a99e464cdf8a4388b8b459e6f0
Tomokazu Imai <bobbyjam99@gmail.com>
2017-04-12 21:10:16 +0900

    イニシャライザで分母0を禁止する

--- kotlin-startbook/src/Rational.kt
+++ kotlin-startbook/src/Rational.kt
@@ -1,5 +1,9 @@
 // クラスRationalの定義
 class Rational(val numerator: Int, val denominator: Int) {
+    // イニシャライザで分母0を禁止する
+    init {
+        require(denominator != 0, {"denominator must not be null"})
+    }
     // toStringをオーバーライド
     override fun toString(): String = "${numerator}/${denominator}"
 }
