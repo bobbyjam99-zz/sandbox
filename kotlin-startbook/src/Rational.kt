b8f38c2e730aba566d7350575d8e1d5caf1c1dcd
Tomokazu Imai <bobbyjam99@gmail.com>
2017-04-12 21:08:07 +0900

    toStringをオーバーライド

--- kotlin-startbook/src/Rational.kt
+++ kotlin-startbook/src/Rational.kt
@@ -1,2 +1,5 @@
 // クラスRationalの定義
-class Rational(val numerator: Int, val denominator: Int)
+class Rational(val numerator: Int, val denominator: Int) {
+    // toStringをオーバーライド
+    override fun toString(): String = "${numerator}/${denominator}"
+}
